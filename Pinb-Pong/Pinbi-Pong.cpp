cfc0d78274e0ac53b827ea0fc5a1d77431c9befc
Rodney <rodney@astralclocktowerstudios.com>
2024-08-27 01:11:23 -0300

    Draw the paddles and the ball

--- Pinb-Pong/Pinbi-Pong.cpp
+++ Pinb-Pong/Pinbi-Pong.cpp
@@ -7,15 +7,22 @@ int main() {
 	
 	// Screen Definitions
 	const int screen_width = 1280;
-	const int screen_height = 720;
+	const int screen_height = 800;
 	InitWindow(screen_width, screen_height, "Pinbi-Pong");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 
-		EndDrawing(); 
+		//Draw Elements
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+		DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
+		DrawRectangle(screen_width-35, screen_height / 2 - 60, 25, 120, WHITE);
+		
 
+
+		EndDrawing(); 
 
 	}
 
