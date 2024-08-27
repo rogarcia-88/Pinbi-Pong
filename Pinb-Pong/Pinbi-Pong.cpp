bb1eda5bb3a90172ff6a2330505a2a635ef8beb4
Rodney <rodney@astralclocktowerstudios.com>
2024-08-27 00:51:58 -0300

    Create blank screen & game loop

--- Pinb-Pong/Pinbi-Pong.cpp
+++ Pinb-Pong/Pinbi-Pong.cpp
@@ -1,15 +1,25 @@
 #include "raylib.h"
 #include "iostream"
 
-using namespace std;
+
 
 int main() {
-	std::cout << "Start the game" << endl;
-
+	
+	// Screen Definitions
 	const int screen_width = 1280;
 	const int screen_height = 720;
-
 	InitWindow(screen_width, screen_height, "Pinbi-Pong");
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		EndDrawing(); 
+
+
+	}
+
+
 
 
 	CloseWindow();
