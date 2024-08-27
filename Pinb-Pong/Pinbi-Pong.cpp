5df17e8f7e3d08d571d701961084b28cff01504d
Rodney <rodney@astralclocktowerstudios.com>
2024-08-27 03:56:35 -0300

    Move the ball around

--- Pinb-Pong/Pinbi-Pong.cpp
+++ Pinb-Pong/Pinbi-Pong.cpp
@@ -1,7 +1,24 @@
 #include "raylib.h"
-#include "iostream"
 
+// Objects Creation
+class Ball {
+public:
+	int x, y;
+	int speed_x, speed_y;
+	float radius;
 
+	void Draw() {
+		DrawCircle(x, y, radius, WHITE);
+	}
+	void Update() {
+		x += speed_x;
+		y += speed_y;
+
+	}
+};
+
+// Objects Instancing
+Ball ball;
 
 int main() {
 	
@@ -11,17 +28,27 @@ int main() {
 	InitWindow(screen_width, screen_height, "Pinbi-Pong");
 	SetTargetFPS(60);
 
+	//Ball Properties
+	ball.radius = 20;
+	ball.x = screen_width / 2;
+	ball.y = screen_height / 2;
+	ball.speed_x = 7;
+	ball.speed_y = 7;
+
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 
+		//Update
+		ball.Update();
+
 		//Draw Elements
+		ClearBackground(BLACK);
 		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
-		DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+		ball.Draw();
 		DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
 		DrawRectangle(screen_width-35, screen_height / 2 - 60, 25, 120, WHITE);
 		
-
-
 		EndDrawing(); 
 
 	}
